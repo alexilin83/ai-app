dfe27a7ba5ac9abd1db714ed3cc83008ea1c96e8
Alex Ilin <alexilin83@gmail.com>
2024-05-07 21:23:26 +0300

    animation fix 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,9 @@
-<script lang="ts">
-	import { gsap } from 'gsap';
-	import { onMount } from 'svelte';
-
-	let introText1: any,
-		introText2: any,
-		introText3: any;
-
-	onMount(() => {
-		gsap.fromTo(introText1, {
-			x: 50,
-			opacity: 0
-		}, {
-			x: 0,
-			opacity: 1,
-			stagger: .05,
-			duration: 2,
-			ease: 'power4.out'
-		});
-		gsap.fromTo(introText2, {
-			x: 50,
-			opacity: 0
-		}, {
-			x: 0,
-			opacity: 1,
-			stagger: .05,
-			duration: 2,
-			delay: .1,
-			ease: 'power4.out'
-		});
-		gsap.fromTo(introText3, {
-			x: 50,
-			opacity: 0
-		}, {
-			x: 0,
-			opacity: 1,
-			stagger: .05,
-			duration: 2,
-			delay: .2,
-			ease: 'power4.out'
-		});
-	});
-</script>
-
 <section class="section section_intro">
 	<div class="container">
 		<h1 class="intro">
-			<span bind:this={introText1} class="intro_text intro_text-title">Alexey ilin</span>
-			<span bind:this={introText2} class="intro_text intro_text-main">Web developer &</span>
-			<span bind:this={introText3} class="intro_text intro_text-other">source explorer</span>
+			<span class="intro_text intro_text-title">Alexey ilin</span>
+			<span class="intro_text intro_text-main">Web developer &</span>
+			<span class="intro_text intro_text-other">source explorer</span>
 		</h1>
 	</div>
 </section>
@@ -56,13 +12,15 @@
 	@import 'variables.scss';
 	.intro {
 		text-transform: uppercase;
-		span {
-			display: block;
-		}
 		&_text {
 			opacity: 0;
+			display: block;
+			animation: 2s text-reveal cubic-bezier(0.16, 1, 0.3, 1) both;
 			&-title {
 				font-weight: 200;
+			}
+			&-main {
+				animation-delay: .1s;
 			}
 			&-other {
 				background: linear-gradient(
@@ -72,6 +30,7 @@
 				);
 				background-clip: text;
 				-webkit-text-fill-color: transparent;
+				animation-delay: .2s;
 			}
 		}
 	}
