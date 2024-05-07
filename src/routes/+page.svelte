8adc0f43c4dc3e275036dc1be64eccfd2ea394ea
Alex Ilin <alexilin83@gmail.com>
2024-05-07 17:36:26 +0300

    animation fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,47 @@
 <script lang="ts">
 	import { gsap } from 'gsap';
 	import { onMount } from 'svelte';
+	import SplitType from 'split-type';
 
 	let introText1: any,
 		introText2: any,
-		introText3: any,
-		introTextCover1: any,
-		introTextCover2: any,
-		introTextCover3: any;
+		introText3: any;
 
 	onMount(() => {
-		const tl1 = gsap.timeline();
-		const tl2 = gsap.timeline({ delay: 0.2 });
-		const tl3 = gsap.timeline({ delay: 0.4 });
-		tl1.fromTo(
-			introTextCover1,
-			{
-				x: '-100%'
-			},
-			{
-				x: 0,
-				duration: 0.5,
-				ease: 'power1.out'
-			}
-		);
-		tl1.set(introText1, {
-			opacity: 1
+		const text1 = new SplitType(introText1);
+		const text2 = new SplitType(introText2);
+		const text3 = new SplitType(introText3);
+		gsap.fromTo(text1.chars, {
+			x: 50,
+			opacity: 0
+		}, {
+			x: 0,
+			opacity: 1,
+			stagger: .05,
+			duration: 2,
+			ease: 'power4.out'
 		});
-		tl1.to(introTextCover1, {
-			x: '100%',
-			duration: 0.5,
-			ease: 'power1.out'
+		gsap.fromTo(text2.chars, {
+			x: 50,
+			opacity: 0
+		}, {
+			x: 0,
+			opacity: 1,
+			stagger: .05,
+			duration: 2,
+			delay: .2,
+			ease: 'power4.out'
 		});
-		tl2.fromTo(
-			introTextCover2,
-			{
-				x: '-100%'
-			},
-			{
-				x: 0,
-				duration: 0.5,
-				ease: 'power1.out'
-			}
-		);
-		tl2.set(introText2, {
-			opacity: 1
-		});
-		tl2.to(introTextCover2, {
-			x: '100%',
-			duration: 0.5,
-			ease: 'power1.out'
-		});
-		tl3.fromTo(
-			introTextCover3,
-			{
-				x: '-100%'
-			},
-			{
-				x: 0,
-				duration: 0.5,
-				ease: 'power1.out'
-			}
-		);
-		tl3.set(introText3, {
-			opacity: 1
-		});
-		tl3.to(introTextCover3, {
-			x: '100%',
-			duration: 0.5,
-			ease: 'power1.out'
+		gsap.fromTo(text3.chars, {
+			x: 50,
+			opacity: 0
+		}, {
+			x: 0,
+			opacity: 1,
+			stagger: .05,
+			duration: 2,
+			delay: .4,
+			ease: 'power4.out'
 		});
 	});
 </script>
@@ -76,18 +49,9 @@
 <section class="section section_intro">
 	<div class="container">
 		<h1 class="intro">
-			<span class="intro_text-container">
-				<span bind:this={introText1} class="intro_text intro_text-title">Alexey ilin</span>
-				<span bind:this={introTextCover1} class="intro_text-cover"></span>
-			</span>
-			<span class="intro_text-container">
-				<span bind:this={introText2} class="intro_text intro_text-main">Web developer &</span>
-				<span bind:this={introTextCover2} class="intro_text-cover"></span>
-			</span>
-			<span class="intro_text-container">
-				<span bind:this={introText3} class="intro_text intro_text-other">source explorer</span>
-				<span bind:this={introTextCover3} class="intro_text-cover"></span>
-			</span>
+			<span bind:this={introText1} class="intro_text intro_text-title">Alexey ilin</span>
+			<span bind:this={introText2} class="intro_text intro_text-main">Web developer &</span>
+			<span bind:this={introText3} class="intro_text intro_text-other">source explorer</span>
 		</h1>
 	</div>
 </section>
@@ -97,14 +61,10 @@
 	.intro {
 		text-transform: uppercase;
 		span {
-			display: inline-flex;
-		}
-		&_text-container {
-			position: relative;
-			overflow: hidden;
+			display: block;
 		}
 		&_text {
-			opacity: 0;
+			// opacity: 0;
 			&-title {
 				font-weight: 200;
 			}
@@ -117,15 +77,6 @@
 				background-clip: text;
 				-webkit-text-fill-color: transparent;
 			}
-		}
-		&_text-cover {
-			position: absolute;
-			left: 0;
-			top: 0;
-			width: 100%;
-			height: 100%;
-			background: #fff;
-			transform: translateX(-100%);
 		}
 	}
 </style>
