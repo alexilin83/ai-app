ef2ce9f4d632f7597ec95d6de06d8c8b4d9e94f7
Alex Ilin <alexilin83@gmail.com>
2025-02-03 19:23:34 +0300

    old build dir

---
+++ build/_app/immutable/entry/start.CEhos2xU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.n_FtbjKN.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-		}),
+		adapter: adapter(),
 	},
 };
 
