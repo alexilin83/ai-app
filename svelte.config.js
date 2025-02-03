5f2392bfdf21b60213d6352784c2369522599852
Alex Ilin <alexilin83@gmail.com>
2025-02-03 23:44:49 +0300

    test deploy 3

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'ai-app'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 };
